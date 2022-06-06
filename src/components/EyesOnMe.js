fffb9ace69886ad69cf5718a4a9e522cb3c6b07e
Lian <biakhong@gmail.com>
2022-06-05 22:15:09 -0400

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+import react from "react";
+import ReactDOM from 'react-dom';
+
+
+
+function EyesOnMe(){
+    return <button onBlur={()=>console.log("Hey! Eyes on me!")} onFocus={()=>console.log("Good!")}>Eyes on me</button>;
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import react from "react";
+import ReactDOM from 'react-dom';
+
+
+function handleChange() {
+    console.log("Entering password...");
+  }
+
+function Keypad(){
+    return <input type="password"  onChange={handleChange} />
+}
+export default Keypad;
